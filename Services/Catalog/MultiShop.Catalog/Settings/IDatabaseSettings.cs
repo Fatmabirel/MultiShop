d50253326ba0d1854df78f4209962222eacc90c8
Fatma Birel <fatmabrl11@gmail.com>
2024-09-03 21:45:26 +0300

    Implement DatabaseSettings

---
+++ Services/Catalog/MultiShop.Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        string CategoryCollectionName { get; set; }
+        string ProductCollectionName { get; set; }
+        string ProductDetailCollectionName { get; set; }
+        string ProductImageCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
